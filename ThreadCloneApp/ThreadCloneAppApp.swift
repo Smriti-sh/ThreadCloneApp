5c7053fc00ac8fd435cceb4c4679b959049ea779
Smriti-sh <smritisapr2001@gmail.com>
2025-01-27 08:28:49 +0530

    Initial Commit

---
+++ ThreadCloneApp/ThreadCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadCloneAppApp.swift
+//  ThreadCloneApp
+//
+//  Created by Smriti Sharma on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
